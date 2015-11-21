5dec1cf66334451e1f077eaaf1c6861b1af56f0c
aniruddha-chakraborty <aniruddha.app@gmail.com>
2015-11-21 21:12:49 +0600

    updated verticalmirror

--- src/Layouts/VerticalMirror.php
+++ src/Layouts/VerticalMirror.php
@@ -1,10 +1,8 @@
 <?php
 // VerticalMirror.php
 namespace Hedronium\Avity\Layouts;
-
 use Hedronium\Avity\Layout;
 use Hedronium\Avity\Generator;
-
 class VerticalMirror extends Layout
 {
   	public $rows = 5;
@@ -12,30 +10,23 @@ class VerticalMirror extends Layout
     public function drawGrid(Generator $gen)
     {
 		$grid = [];
-
         $max_columns = $this->columns/2;
-
         if ($this->columns&1 === 0) {
               $max_columns -= 1;
         }
-
-      	$max_columns = floor($max_columns);
-
+      	$max_columns = ceil($max_columns);
         for ($y = 0; $y < $this->rows; $y++) {
             // This will store the column value of the row
             $grid[$y] = [];
-
           	for ($x = 0; $x < $this->columns; $x++) {
                 $grid[$y][$x] = false;
             }
-
             for ($x = 0; $x < $max_columns; $x++) {
                   $value = $gen->shouldDraw($x, $y);
                   $grid[$y][$x] = $value;
                   $grid[$y][($this->columns-1)-$x] = $value;
             }
         }
-
         return $grid;
     }
 }
